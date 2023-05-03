09bdf232ff9e477163ff818924141b3c36d00686
rattea <132305908+rattea@users.noreply.github.com>
2023-05-03 20:00:47 +0000

    3.3 page 104

---
+++ HelloName2.cpp
@@ -0,0 +1,13 @@
+#include "iostream"
+
+int main(){
+	std::cout<<"Введи свое имя \n";
+	std::string name;
+	std::cin>>name;
+	std::cout<<"Введи свой возраст \n";
+	int age;
+	std::cin>>age;
+	std::cout<<"QQ человек по имени "<<name<<" и возрастом "<<age<<"\n";
+	return 0;
+}
+
